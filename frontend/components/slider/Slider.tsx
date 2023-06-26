b3395af49f71ff415890cb7e57f8ab4a6f9b76b3
Polina7777 <polina.produho98@mail.ru>
2023-06-27 01:14:31 +0200

    fix:fix slider

--- frontend/components/slider/Slider.tsx
+++ frontend/components/slider/Slider.tsx
@@ -5,6 +5,9 @@ import StepIndicator from 'react-native-step-indicator';
 import { textPrimary, textSecondary } from '../../constants/Colors';
 import { heightScreen, widthScreen } from '../../constants/Sizes';
 import { TouchableHighlight } from 'react-native';
+import { Image } from 'expo-image';
+import { useState } from 'react';
+import Animated, { useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
 
 const stepIndicatorStyles = {
@@ -29,16 +32,36 @@ const stepIndicatorStyles = {
 };
 
 export default function VerticalStepIndicator({data}:any) {
-  const [currentPage, setCurrentPage] = React.useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const [check, setCheck] = useState(false)
   const viewabilityConfig = React.useRef({ itemVisiblePercentThreshold: 40 })
     .current;
 
   const renderPage = (rowData: any) => {
+  
     const item = rowData.item;
     return (
        <TouchableHighlight>
       <View style={styles.rowItem}>
+        <View style = {{flexDirection:'row',position:'relative'}}>
+      <Image  key={item.id}
+                      style={{
+                        width: 30,
+                        height: 30,
+                        justifyContent: "center",
+                        position:'absolute',
+                        left:-25,
+                        top:10
+                      }}
+                      source={{
+                        uri: check
+                          ? item.attributes.check
+                          : item.attributes.uncheck,
+                      }}
+                      />
+                     
         <Text style={styles.title}>{item.attributes.name}</Text>
+        </View>
         <Text style={styles.body}>{item.attributes.description}</Text>
       </View>
      </TouchableHighlight>  
@@ -52,11 +75,44 @@ export default function VerticalStepIndicator({data}:any) {
     }
   }, []);
 
+
+  // const handler = useAnimatedScrollHandler(
+  //   {
+  //     onEndDrag: (e) => {
+  //       sv.value = state;
+  //     },
+  //   },
+  //   dependencies
+  // );
+  const width = 135.5;
+
+ 
+
+  const translationY = useSharedValue(0); 
+  
+  
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    translationY.value = event.contentOffset.y;
+  });
+
+  const stylez = useAnimatedStyle(() => {
+    let heightOfSlider = translationY.value 
+
+    return {
+      height: heightOfSlider< 0 ? 0 : heightOfSlider
+      // transform: [
+      //   {
+      //     translateY: translationY.value,
+      //   },
+      // ],
+    };
+  });
+
   return (
-    <View style={styles.container}>
+    <Animated.View style={[styles.container]}>
    
-      <View style={{height:height/3,flexDirection:'row', width:width-20, padding:10}}>
-      <View style={styles.stepIndicator}>
+      {/* <View style={{height:height/3,flexDirection:'row', width:width-20, padding:10}}> */}
+      {/* <View style={styles.stepIndicator}>
 
         <StepIndicator
           customStyles={stepIndicatorStyles}
@@ -64,22 +120,30 @@ export default function VerticalStepIndicator({data}:any) {
           direction="vertical"
           currentPosition={currentPage}
         />
-      </View>
+      </View> */}
     
-      <FlatList
+<Animated.View  style={[{ borderLeftWidth:4,
+            borderLeftColor:'yellow',height:height/5},stylez]}/>
+            
+      <Animated.FlatList style={{ borderLeftWidth:4,
+    borderLeftColor:'blue',height:height/3,paddingRight:10}}
         // style={{flexGrow:1}}
+        onScroll={scrollHandler}
         data={data}
         renderItem={renderPage}
         onViewableItemsChanged={onViewableItemsChanged}
         // scrollEnabled={true}
         scrollToOverflowEnabled={true}
       />
-   
-      </View>
+
+ 
+      {/* </View> */}
       
-    </View>
+    </Animated.View>
   );
 }
+const colorCheck = 'https://www.svgrepo.com/show/468260/check-circle.svg';
+const uncheck = 'https://www.svgrepo.com/show/470763/check-circle.svg';
 const {width} = widthScreen;
 const {height} = heightScreen;
 const styles = StyleSheet.create({
@@ -89,28 +153,35 @@ const styles = StyleSheet.create({
     backgroundColor: 'transition',
     paddingHorizontal:30,
      width:width-20,
+     position:'relative',
+     height:height/3
+    
+    
   },
   stepIndicator: {
     // flex:1,
-   paddingHorizontal:20,
+  //  paddingHorizontal:20,
   },
   rowItem: {
     paddingLeft:10,
+    alignItems:'flex-start'
+    // borderLeftWidth:4,
+    // borderLeftColor:'blue'
     //  flex: 3,
     // paddingVertical: 10,
   },
   title: {
-    flex: 1,
+    // flex: 1,
     fontSize: 15,
     color: textSecondary,
     paddingVertical: 20,
     fontWeight: '600',
   },
   body: {
-     flex: 1,
+    //  flex: 1,
     fontSize: 13,
     color: textSecondary,
-     width:width/2,
+     width:width-70,
     // lineHeight: 12,
     // marginRight: 8,
     marginBottom:10
